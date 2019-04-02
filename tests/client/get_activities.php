e8253c13fe011f3a3360a90895a882850b2a7498
Sébastien Fraysse <sebastien@fraysse.eu>
2019-04-02 10:57:13 +0200

    updated test scripts

--- tests/client/get_activities.php
+++ tests/client/get_activities.php
@@ -2,22 +2,25 @@
 
 
 /// SETUP - NEED TO BE CHANGED
-$domainname = 'http://moodle35.test';
-$functionname = 'logstore_trax_get_activities';
-$token = '9437ea259fc079a1ec66bc46c43c2df2';
+
+$domainname = 'http://moodle35.test';           // Moodle root URL
+
+$token = '9437ea259fc079a1ec66bc46c43c2df2';    // Generated in Moodle: Admin > Plugins > Web Services > Manage Tokens
 
 $params = ['items' => [
-    ['type' => 'system', 'id' => 0],
-    ['type' => 'course', 'id' => 2],
-    ['type' => 'lti', 'id' => 1]
+    ['type' => 'system', 'id' => 0],            // To get the platform instance. The ID is always 0
+    ['type' => 'course', 'id' => 2],            // To get a course. The ID is visible in the course URL (e.g. http://moodle35.test/course/view.php?id=2)
+    ['type' => 'lti', 'id' => 1]                // To get an LTI activity. The ID is not visible in the URL (not the same ID!). Check your DB "lti" table :(
 ]];
 
+
 /// REST CALL
+
 header('Content-Type: text/plain');
+$functionname = 'logstore_trax_get_activities';
 $serverurl = $domainname . '/webservice/rest/server.php'. '?moodlewsrestformat=json&wsfunction='.$functionname . '&wstoken=' . $token;
 require_once('./curl.php');
 $curl = new curl;
 $items = $curl->post($serverurl, $params);
 print_r($items);
-die;
 
--- tests/client/get_actors.php
+++ tests/client/get_actors.php
@@ -2,19 +2,22 @@
 
 
 /// SETUP - NEED TO BE CHANGED
-$domainname = 'http://moodle35.test';
-$functionname = 'logstore_trax_get_actors';
-$token = '9437ea259fc079a1ec66bc46c43c2df2';
+
+$domainname = 'http://moodle35.test';           // Moodle root URL
+
+$token = '9437ea259fc079a1ec66bc46c43c2df2';    // Generated in Moodle: Admin > Plugins > Web Services > Manage Tokens
 
 $params = ['items' => [
-    ['type' => 'user', 'id' => 2]
+    ['type' => 'user', 'id' => 2]               // To get a user. The ID is visible in the user URL (http://moodle35.test/user/view.php?id=2)
 ]];
 
+
 /// REST CALL
+
 header('Content-Type: text/plain');
+$functionname = 'logstore_trax_get_actors';
 $serverurl = $domainname . '/webservice/rest/server.php'. '?moodlewsrestformat=json&wsfunction='.$functionname . '&wstoken=' . $token;
 require_once('./curl.php');
 $curl = new curl;
 $items = $curl->post($serverurl, $params);
 print_r($items);
-die;
