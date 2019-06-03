e4d0576b0df7485aacf488217396d598147c19b9
Sébastien Fraysse <sebastien@fraysse.eu>
2019-06-03 18:52:51 +0200

    LRS proxy

---
+++ proxy/statements_get.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * LRS proxy.
+ *
+ * @package    logstore_trax
+ * @copyright  2019 Sébastien Fraysse {@link http://fraysse.eu}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../../../../../config.php');
+require_login();
+
+$controller = new \logstore_trax\src\controller();
+
+// Force the agent and related_agents for security reasons.
+// ...
+
+// Convert the activity ID.
+// ...
+
+// Get the statements.
+$response = $controller->client()->statements()->get($_GET);
+
+// Return error.
+if ($response->code != 200) {
+    http_response_code($response->code);
+    die;
+}
+
+// Return JSON.
+header('Content-Type: application/json');
+header('X-Experience-API-Version: ' . $response->headers->xapi_version);
+header('X-Experience-API-Consistent-Through: ' . $response->headers->xapi_consistent_through);
+echo json_encode($response->content);
+
+
+
+
+
---
+++ proxy/statements_post.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * LRS proxy.
+ *
+ * @package    logstore_trax
+ * @copyright  2019 Sébastien Fraysse {@link http://fraysse.eu}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../../../../../config.php');
+require_login();
+
+$controller = new \logstore_trax\src\controller();
+
+// Get data.
+$input = file_get_contents('php://input');
+$data = json_decode($input, true);
+if (!$data) {
+    http_response_code(400);
+    die;
+}
+
+// Force the agent.
+// ...
+
+// Convert the activity ID.
+// ...
+
+// Get the statements.
+$response = $controller->client()->statements()->post($data);
+
+// Return error.
+if ($response->code != 200) {
+    http_response_code($response->code);
+    die;
+}
+
+// Return JSON.
+header('Content-Type: application/json');
+header('X-Experience-API-Version: ' . $response->headers->xapi_version);
+echo json_encode($response->content);
+
+
+
+
+
