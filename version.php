b9c42e49ff205b3ec4f1bd92bd875bec954bde65
Sébastien Fraysse <sebastien@fraysse.eu>
2019-06-10 14:18:21 +0200

    v0.9

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018050808;
+$plugin->version = 2018050809;
 $plugin->requires = 2018050800;
 $plugin->component = 'logstore_trax';
 
-$plugin->release = 'v0.8';
+$plugin->release = 'v0.9';
 $plugin->maturity = MATURITY_ALPHA;
