de5f5cd6e78933a9adb9ebe7ff4c26a39e7b8f9a
sfraysse <sebastien@fraysse.eu>
2021-04-07 22:51:53 +0200

    v20

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018050819;
+$plugin->version = 2018050820;
 $plugin->requires = 2018050800;
 $plugin->component = 'logstore_trax';
 
-$plugin->release = 'v0.19';
+$plugin->release = 'v0.30';
 $plugin->maturity = MATURITY_ALPHA;
