b562750172a296266af3b6a8d809613bc855ff24
Tim Volckmann <tim.volckmann@mastersolution.com>
2023-11-02 17:15:41 +0100

    fix: deprecated warning passing null to http_build_query() in guzzlehttp

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,8 +7,8 @@ namespace Composer\Autoload;
 class ComposerStaticInit4b32e74a76804daf7097ee269c080157
 {
     public static $files = array (
-        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
@@ -16,6 +16,7 @@ class ComposerStaticInit4b32e74a76804daf7097ee269c080157
         'P' => 
         array (
             'Psr\\Http\\Message\\' => 17,
+            'Psr\\Http\\Client\\' => 16,
         ),
         'G' => 
         array (
@@ -28,7 +29,12 @@ class ComposerStaticInit4b32e74a76804daf7097ee269c080157
     public static $prefixDirsPsr4 = array (
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'Psr\\Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-client/src',
         ),
         'GuzzleHttp\\Psr7\\' => 
         array (
@@ -44,11 +50,16 @@ class ComposerStaticInit4b32e74a76804daf7097ee269c080157
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit4b32e74a76804daf7097ee269c080157::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit4b32e74a76804daf7097ee269c080157::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit4b32e74a76804daf7097ee269c080157::$classMap;
 
         }, null, ClassLoader::class);
     }
